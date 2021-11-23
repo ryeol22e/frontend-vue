848550e70d4a5414373c536acbfee28c9b44719e
ryeol22e <ryeol22e@gmail.com>
2021-11-23 21:40:08 +0900

    vue-compnents-buttons

---
+++ src/components/buttons/submit-button.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <button>{{buttonName}}</button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name : submit-button,
+        data : function() {
+            return {
+                buttonName : '확인'
+            };
+        },
+        created() {
+            console.log('button start..');
+        },
+        methods : {
+
+        }
+    };
+</script>
+
+<style>
+
+</style>
