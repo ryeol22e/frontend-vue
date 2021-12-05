2fca63aa8ebd9c485f8c1a331f05aa037a6b903c
ryeol22e <ryeol22e@gmail.com>
2021-12-05 16:07:48 +0900

    main vue init

---
+++ src/views/main/Main.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <b-nav tabs>
+            <b-nav-item active>Active</b-nav-item>
+            <b-nav-item>Link</b-nav-item>
+            <b-nav-item>Another Link</b-nav-item>
+            <b-nav-item disabled>Disabled</b-nav-item>
+        </b-nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'Main',
+        data : function() {
+            return {
+
+            }
+        },
+        created : function() {
+            
+        }
+    }
+</script>
+
+<style>
+
+</style>
