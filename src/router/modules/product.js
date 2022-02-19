7e5c56eb1c564414555c67901e350e29113ca960
ryeol22e <ryeol22e@gmail.com>
2022-02-19 16:42:23 +0900

    소스정리

--- src/router/modules/product.js
+++ src/router/modules/product.js
@@ -1,7 +1,7 @@
 export default [
-    {
-        path : '/products/detail/:prdtNo',
-        name : 'ProductDetail',
-        component : ()=> import('@/views/product/ProductDetail.vue')
-    },
+	{
+		path : '/products/detail/:prdtNo',
+		name : 'ProductDetail',
+		component : ()=> import('@/views/product/ProductDetail.vue')
+	},
 ];
